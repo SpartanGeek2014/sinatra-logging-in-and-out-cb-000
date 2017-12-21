fcd024459da2ac614ae2821174110b1d0c01bb1b
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-21 23:59:46 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221040620) do
+ActiveRecord::Schema.define(version: 20171221235233) do
 
 end
