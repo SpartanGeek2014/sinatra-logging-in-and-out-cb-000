98bcdf1c511a6d51239bead6baa0c169e74bc36d
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-22 01:27:48 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20171222012323) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "balance"
+    t.decimal "balance"
   end
 
 end
