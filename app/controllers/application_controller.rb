45069679eda1616505ab9489af217cc111585095
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-22 01:51:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
-    end
+    else
     erb :error
   end
 
